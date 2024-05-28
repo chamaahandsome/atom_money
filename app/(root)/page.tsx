9c9558b23362433bd771358da91e69080c69c0a8
chamaahandsome <timboywa@gmail.com>
2024-05-28 17:47:37 -0400

    Update import statement in layout.tsx and add header and total balance components in page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import HeaderBox from '../../components/HeaderBox'
+import TotalBalanceBox from '../../components/TotalBalanceBox'
 
 const HomePage = () => {
+const loggedIn = { firstName: "Bazu"}
+
   return (
-    <div>
-      HomePage
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+            <HeaderBox
+            type="greeting"
+            title="Welcome to Atom"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Your personal finance assistant"
+            />
+            <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={125000.24}
+            />
+        </header>
+      </div>
+    </section>
   )
 }
 
-export default HomePage;
+export default HomePage
