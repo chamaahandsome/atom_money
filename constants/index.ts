98a94d27009ab57ffe4d4f267a027bd0146dc54d
chamaahandsome <timboywa@gmail.com>
2024-06-03 22:20:01 -0400

    Add Sentry configuration file and update package.json to include @sentry/nextjs package

--- constants/index.ts
+++ constants/index.ts
@@ -19,6 +19,11 @@ export const sidebarLinks = [
     route: "/payment-transfer",
     label: "Transfer Funds",
   },
+  {
+    imgURL: "/icons/empty-wallet.svg",
+    route: "/wallet",
+    label: "Wallet",
+  },
 ];
 
 // good_user / good_password - Bank of America
